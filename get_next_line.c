6515cb07c948739f1c320847f37e693372c73c9a
Mohamed Amine Boukour <mboukour@e2r3p2.1337.ma>
2023-12-01 21:25:08 +0100

    fixed some leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mboukour <mboukour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 08:49:32 by mboukour          #+#    #+#             */
-/*   Updated: 2023/12/01 15:53:31 by mboukour         ###   ########.fr       */
+/*   Updated: 2023/12/01 21:14:32 by mboukour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 
 char *get_next_line(int fd)
 {
-    static char *save = NULL;
+    static char *save;
     static int reached_last = 0;
     char *buffer;
     char *newline;
     char *line;
     int bytes_read;
+    char *temp;
 
     if ((fd < 0 || fd > OPEN_MAX) || BUFFER_SIZE <= 0)
         return (NULL);
     buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
     if (!buffer)
         return (NULL);
-    buffer[BUFFER_SIZE] = '\0';
     newline = ft_strchr(save, '\n');
     while(!newline)
     {
@@ -35,41 +35,71 @@ char *get_next_line(int fd)
         if (bytes_read == -1)
         {
             free(buffer);
+            buffer = NULL;
+            free(save);
+            save= NULL;
             return (NULL);
         }
         if (bytes_read == 0)
         {
             free(buffer);
-            if (!reached_last && save != NULL)
+            buffer = NULL;
+            if (!reached_last && save != NULL && save[0] != '\0')
             {
                 line = ft_strdup(save);
+                if(!line)
+                {
+                    free(save);
+                    save = NULL;
+                    return (NULL);
+                }
                 reached_last = 1;
+                free(save);
+                save = NULL;
                 return (line);
             }
             return (NULL);
         }
         buffer[bytes_read] = '\0';
-        char *temp = save;
+        temp = save;
         save = ft_strjoin(save, buffer);
+        if(!save)
+            {
+                free(temp);
+                return (NULL);
+            }
         free(temp);
         newline = ft_strchr(save, '\n');
     }
     free(buffer);
-
+    buffer = NULL;
     if(newline)
     {
         line = ft_substr(save, 0, (newline - save) + 1);
-        char *temp = save;
+        if(!line)
+            {
+                free(save);
+                save = NULL;
+                return (NULL);
+            }
+        temp = save;
         save = ft_strdup(newline + 1);
+        if(!save)
+            {
+                free(temp);
+                return (NULL);
+            }
         free(temp);
-        if (!save)
-            return (NULL);
     }
     else
     {
         line = ft_strdup(save);
         if(!line)
+        {
+            free(save);
+            save = NULL;
             return (NULL);
+        }
     }
     return (line);
 }
